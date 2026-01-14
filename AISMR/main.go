51476bff9f076ade525e2fdcdfae3bdad27a6de1
Viki <94736133+Leovikii@users.noreply.github.com>
2026-01-14 14:32:45 +0800

    Update main.go

--- AISMR/main.go
+++ AISMR/main.go
@@ -18,22 +18,20 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "AISMR",
-		Width:  1024,
-		Height: 768,
-		// 保持无边框
+		Title:     "AISMR",
+		Width:     1024,
+		Height:    768,
 		Frameless: true,
 
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 0},
+		BackgroundColour: &options.RGBA{R: 18, G: 18, B: 20, A: 0},
 
-		// 【关键修改】配置拖放行为
 		DragAndDrop: &options.DragAndDrop{
-			EnableFileDrop:     true,                  // 启用 Wails 的文件拖放事件
-			DisableWebViewDrop: true,                  // 禁用 WebView 默认的“打开文件”行为
-			CSSDropProperty:    "--wails-drop-target", // 可选：用于 CSS 样式的标记
+			EnableFileDrop:     true,
+			DisableWebViewDrop: true,
+			CSSDropProperty:    "--wails-drop-target",
 			CSSDropValue:       "drop",
 		},
 
@@ -46,7 +44,7 @@ func main() {
 			WindowIsTranslucent:  true,
 			BackdropType:         windows.Mica,
 			Theme:                windows.Dark,
-			DisableWindowIcon:    true,
+			DisableWindowIcon:    false,
 		},
 	})
 
